a9fa0d42c4442c5b2b6fb76c941fd63e22d4f48e
emildasolutionspvtltd <61912806+emildasolutionspvtltd@users.noreply.github.com>
2025-07-18 15:32:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
-/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  basePath: '/landingpage',
-  assetPrefix: '/landingpage',
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  typescript: { ignoreBuildErrors: true },
 };
 
 module.exports = nextConfig;
